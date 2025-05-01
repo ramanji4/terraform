70cec0da0cf5809f45cc5bcbc57859e34c23a627
ramanji4 <ramanji181818@gmail.com>
2025-05-01 17:59:00 +0530

    terraform

--- expense/outputs.tf
+++ expense/outputs.tf
@@ -1,6 +1,3 @@
 output "instances_info" {
     value = aws_instance.expense
 }
-
-#we can use both declarations as inputs to create other AWS resources
-#output.instances_info == aws_instance.expense
--- expense/r53.tf
+++ expense/r53.tf
@@ -1,7 +1,7 @@
 resource "aws_route53_record" "expense" {
   count = length(var.instance_names)
   zone_id = var.zone_id
-  name = "${var.instance_names[count.index]}.${var.domain_name}"
+  name = "${var.instance_names[count.index]}.${var.domain_name}" #mysql.ram4india.space 
   type    = "A"
   ttl     = 1
   records = [aws_instance.expense[count.index].private_ip]
