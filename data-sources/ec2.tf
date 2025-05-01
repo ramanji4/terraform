326e1a5bf4b999e2f3d8e7aaf6c2059b0416fd70
ramanji4 <ramanji181818@gmail.com>
2025-05-01 20:53:06 +0530

    terraform

---
+++ data-sources/ec2.tf
@@ -0,0 +1,35 @@
+resource "aws_security_group" "allow_ssh_connection" {
+  name        = "allow_ssh"
+  description = "Allow port number 22 for SSH protocol connection"
+  
+  egress {      #egress is outgoing traffic
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  ingress {      #ingress is incoming traffic to the server
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]  #allow from everyone 
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  tags = {
+    Name = "allow_ssh"
+  }
+}
+
+
+resource "aws_instance" "terraform" {
+  ami           = data.aws_ami.DevOps-Practice.id
+  instance_type = "t3.micro"
+  vpc_security_group_ids = [aws_security_group.allow_ssh_connection.id]
+
+  tags = {
+    Name = "terraform"
+  }
+}
