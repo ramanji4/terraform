3eb221ee0f9e728e2e1144fddaa7d8805727ae68
ramanji4 <ramanji181818@gmail.com>
2025-05-02 18:12:33 +0530

    terraform

---
+++ provisioners/ec2.tf
@@ -0,0 +1,69 @@
+resource "aws_security_group" "allow_ssh_connection" {
+  name        = "allow_ssh"
+  description = "Allow port number 22 and 80"
+  
+  egress {      #egress is outgoing traffic
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  ingress {      #ingress is incoming traffic to the server
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  ingress {     
+    from_port        = 80
+    to_port          = 80
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  tags = {
+    Name = "allow_ssh"
+  }
+}
+
+
+resource "aws_instance" "terraform" {
+  ami           = "ami-09c813fb71547fc4f"
+  instance_type = "t3.micro"
+  vpc_security_group_ids = [aws_security_group.allow_ssh_connection.id]
+
+  tags = {
+    Name = "terraform"
+  }
+
+  provisioner "local-exec" {
+    command = "echo The server's IP address is ${self.public_ip} > public_ip.txt"
+  }
+
+  connection {
+    type     = "ssh"
+    user     = "ec2-user"
+    password = "DevOps321"
+    host     = self.public_ip
+  }
+
+  provisioner "remote-exec" {
+    inline = [
+      "sudo dnf install ansible -y",
+      "sudo dnf install nginx -y",
+      "sudo systemctl start nginx",
+    ]
+  }
+
+  provisioner "remote-exec" {
+    when = destroy
+    inline = [
+      "sudo systemctl stop nginx",
+    ]
+  }
+}
